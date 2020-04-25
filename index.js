832f5de8ff1b913f4e61364e7fd9f9c4ce6bdde7
k-consult <fakorede@codecradle.co>
2020-04-25 15:14:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//write hello world program
+//print out hello world to the screen or console
+//how to print out to console in javascript
+//print out a greeting with a name
+
+var name = "John Doe";
+
+console.log(" Hello world " + name);
