963c5cab2c1785efa534c0bd44e9b7b57179a791
k-consult <fakorede@codecradle.co>
2020-04-25 15:28:54 +0300

    Git commands

--- index.js
+++ index.js
@@ -3,6 +3,24 @@
 //how to print out to console in javascript
 //print out a greeting with a name
 
+//Datatypes
+//String example, "dunsin fakorede"
+//numbers e.g  float or imterger e.g 12.02 , 12
+//Arrays
+
+
+//why do we need git(github,gitlab,bitbucket)
+//(it is keeping track of our revisions)
+//(it helps team to collaborate)
+//(it helps to make changes)
+
+
+//accept the name
 var name = "John Doe";
 
-console.log(" Hello world " + name);
+//prepare the greeting
+var greeting = "Hello" + name;
+
+
+console.log(greeting);
+console.log("End of class");
