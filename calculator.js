4789f95826fabb69f95aa6f8a33103372eebc384
k-consult <fakorede@codecradle.co>
2020-04-27 21:11:34 +0300

    calculator operation

---
+++ calculator.js
@@ -0,0 +1,21 @@
+/*var a = 1;
+var b = 4;
+var sum = a + b;
+console.log(sum);*/
+
+/*function sumFunc(number1, number2) {
+return (number1 + number2);
+}
+console.log(sumFunc(9, 4));*/
+
+
+var x = 3;
+var remainder = x % 2;
+
+if (remainder != 0) {
+    console.log("Number " + x + "is odd");
+} else {
+    console.log("Number " + x + "is even");
+}
+
+function OddEvenNumbers(num);
