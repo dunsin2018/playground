9af1ddf2a0676143981e900d91cf26ff82fffe90
k-consult <fakorede@codecradle.co>
2020-04-29 20:04:16 +0300

    task work

---
+++ task1.js
@@ -0,0 +1,12 @@
+//creat a person object with attributename, age, sex 
+
+var person = {
+    name: "Dunsin",
+    age: "15",
+    sex: "Male"
+}
+
+function personData(person) {
+    return person;
+}
+console.log(person);
